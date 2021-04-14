5fc7ded533b3e9c49e26bf6e4b5bedc0a6ef8d7c
VD <deploy@travis-ci.org>
2021-04-14 10:42:21 +0000

    Deploy daraduda/daraduda.github.io to github.com/daraduda/daraduda.github.io.git:master

---
+++ widgets/jquery/social-buttons/social-button.js
@@ -0,0 +1,19 @@
+(function($) {
+    $(document).ready(function () {
+        $(".reg-extlogin .extlogin-provider").addClass('colorizable');
+        $(".partners .partner").addClass('colorizable');
+        $(".colorizable").not('.colorized').hover(
+            function () {
+                $(this).clone().addClass('colorized').removeClass('colorizable').appendTo($(this));
+            },
+            function () {
+                $(this)
+                    .find('.colorized')
+                    .fadeOut(
+                        500,
+                        function() { $(this).remove(); }
+                    );
+            }
+        );
+    });
+})(jQuery);
